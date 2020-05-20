56639817fa7d3a876e47ac2c91d2abbb30ef59a4
Samuel <samuel.gherardi@gmail.com>
2020-05-20 11:30:54 +0200

    Add the Classroom and Subjects tables to the DB

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,5 +12,8 @@ namespace EsperiaHelp.Data
             : base(options)
         {
         }
+
+        public DbSet<EsperiaHelp.Models.Classroom> Classroom { get; set; }
+        public DbSet<EsperiaHelp.Models.Subject> Subject { get; set; }
     }
 }
--- Data/Migrations/20200520092850_Add_Classroom&Subject_tables.Designer.cs
+++ Data/Migrations/20200520092850_Add_Classroom&Subject_tables.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EsperiaHelp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EsperiaHelp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200520092850_Add_Classroom&Subject_tables")]
+    partial class Add_ClassroomSubject_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
