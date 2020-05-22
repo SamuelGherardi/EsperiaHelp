92e1d27cb2c4402d7611058de55b1ac48097b9ec
Simone Spreafico <simo.sprea1907@gmail.com>
2020-05-22 15:27:21 +0200

    Aggiunte data annotations nel modello Lesson

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,29 @@ namespace EsperiaHelp.Models
 {
     public class Lesson
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; } //data incontro
+
+        [Required]
+        [Display(Name = "Start time")]
         public DateTime StartTime { get; set; } //ora inizio
+
+        [Required]
+        [Display(Name = "End time")]
         public DateTime EndTime { get; set; } //ora fine
+
+        [Display(Name = "Number of participants")]
         public int N_participants { get; set; } //numero partecipanti alll'incontro
+
+        [Display(Name = "Teacher")]
         public ApplicationUser ApplicationUser { get; set; } //identificativo docente dell'incontro
+
+        [Display(Name = "Classroom")]
         public Classroom Classroom { get; set; } //identificativo dell'aula dove si svolgerà l'incontro
     }
 }
